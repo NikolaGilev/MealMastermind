803a23293038a15ce15760eeedbb32b6e86c56cd
Nikola Gilev <nikola.gilev@codechem.com>
2024-09-01 17:49:18 +0200

    remove build folder and add backend api routes

--- API/random_data.js
+++ API/random_data.js
@@ -1,27 +1,40 @@
 const { faker } = require('@faker-js/faker');
 
-const foodNames = [
-  'Pizza', 'Burger', 'Pasta', 'Salad', 'Sushi', 'Steak', 'Tacos', 'Sandwich', 'Soup', 'Ice Cream',
-  'Chocolate Cake', 'Apple Pie', 'Spaghetti', 'Ramen', 'Pancakes', 'Lasagna', 'Curry', 'Dumplings',
-];
+  const foodNames = [
+    'Pizza', 'Burger', 'Pasta', 'Salad', 'Sushi', 'Steak', 'Tacos', 'Sandwich', 'Soup', 'Ice Cream',
+    'Chocolate Cake', 'Apple Pie', 'Spaghetti', 'Ramen', 'Pancakes', 'Lasagna', 'Curry', 'Dumplings',
+  ];
 
+// Generate random recipes using faker
 function generateRandomRecipes(count = 10) {
   const recipes = [];
-
   for (let i = 0; i < count; i++) {
     const id = faker.number.int({ min: 26, max: 100 });
-    const foodName = faker.helpers.arrayElement(foodNames); 
+    const foodName = faker.helpers.arrayElement(foodNames);
     const title = `${faker.commerce.productAdjective()} ${foodName}`;
     const imageUrl = faker.image.urlLoremFlickr({ category: 'food', query: foodName.split(' ').join('+') });
-    const ingredients = `${faker.number.int({ min: 100, max: 500 })}g ${faker.commerce.productMaterial()}, ${faker.number.int({ min: 1, max: 10 })} ${faker.commerce.productAdjective()} ${foodName}`;
-    const instructions = faker.lorem.paragraphs(3);
-    const nutritionalInfo = `Calories: ${faker.number.int({ min: 200, max: 700 })}, Fat: ${faker.number.int({ min: 5, max: 30 })}g, Carbs: ${faker.number.int({ min: 20, max: 80 })}g, Protein: ${faker.number.int({ min: 10, max: 40 })}g`;
+
+    const ingredients = Array.from({ length: faker.number.int({ min: 2, max: 6 }) }, () => ({
+      name: faker.commerce.productMaterial(),
+      grams: faker.number.int({ min: 50, max: 500 })
+    }));
+
+    const instructions = Array.from({ length: faker.number.int({ min: 3, max: 6 }) }, () => faker.lorem.sentence());
+
+    const nutritionalInfo = {
+      calories: faker.number.int({ min: 200, max: 700 }),
+      fat: faker.number.int({ min: 5, max: 30 }),
+      carbs: faker.number.int({ min: 20, max: 80 }),
+      protein: faker.number.int({ min: 10, max: 40 })
+    };
+
     const rating = faker.number.float({ min: 3, max: 5, multipleOf: 0.1 });
     const author = `${faker.person.firstName()} ${faker.person.lastName()}`;
     const description = faker.lorem.sentence();
     const datePosted = `${faker.date.past().toISOString().split('T')[0]}`;
-    const preparationTime = `${faker.number.int({ min: 10, max: 60 })} minutes`;
-    const likes = `${faker.number.int({ min: 0, max: 100 })}`;
+    const preparationTime = faker.number.int({ min: 10, max: 60 });
+    const likes = faker.number.int({ min: 0, max: 100 });
+    const comments = [];
 
     recipes.push({
       id,
@@ -36,6 +49,7 @@ function generateRandomRecipes(count = 10) {
       datePosted,
       preparationTime,
       likes,
+      comments,
     });
   }
 
